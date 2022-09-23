a00209483b43327846a4f21804a1b4ea5f65b1fd
Tedros Berihu <89820713+tedros100@users.noreply.github.com>
2022-09-23 15:38:14 -0700

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,201 @@
+https://app.codesignal.com/standardized-test/GgvonNCksGzjey46c
+
+function getAverage(score){
+  //Average = sum/count no
+  const count =  score.length;
+  const sum = score.reduce((a,b) => a+b, 0);
+  const average = Math.floor(sum/count) ;
+  
+  return average;
+}  
+
+console.log(getAverage([10,5,2]))
+// deleting the lastfront and the front element
+function removeChar(str){
+  const strArrey= str.split("") 
+  const strSlice = strArrey.slice(1, strArrey.length-1)
+  const newWord =  strSlice.join("")
+   return newWord
+}
+console.log(removeChar("helloo"));
+removeChar("gawena")
+// Centurey caluclation.
+
+function century(years) {
+   
+    if (years % 100 === 0) {
+    return Math.floor(years/100) 
+    } else {
+    return Math.floor((years/100) +1)
+    }
+}
+
+console.log(century(165));
+///////////////////////////////////////////////
+function removeChar(str){
+   
+   
+  const strArrey= str.split("")
+   
+  const strSlice = strArrey.slice(1, strArrey.length-1)
+   const newWord =  strSlice.join("")
+   return newWord
+}
+
+console.log(removeChar("helloo"));
+
+//////////////////////////////////////////////////////////////////////////////////////
+ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+         const age21over = [];
+    for (let i = 0; i < ages.length; i++) {
+        if(ages[i]> 21) {
+        
+          age21over.push(ages[i])
+        }        
+    }
+console.log(age21over)
+
+filter function(age, index, ages) age could be 
+
+const ageover = ages.filter((age) => {
+    if(age >= 21) {
+        return true
+    }
+})
+console.log(age21over)
+///////////////////////////////////////////////////////////////////////////////////////////
+const companies= [
+  {name: "Company One", category: "Finance", start: 1981, end: 2004},
+  {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+  {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+  {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+  {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+  {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+  {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+  {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+  {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+];
+
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+for(let i = 0; i < companies.length; i++) {
+  console.log(companies[i]);
+}
+
+forEach
+
+companies.forEach(function(company) {
+  console.log(company.name);
+});
+
+filter
+
+Get 21 and older
+
+let canDrink = [];
+for(let i = 0; i < ages.length; i++) {
+  if(ages[i] >= 21) {
+    canDrink.push(ages[i]);
+  }
+}
+
+const canDrink = ages.filter(function(age) {
+  if(age >= 21) {
+    return true;
+  }
+});
+
+const canDrink = ages.filter(age => age >= 21);
+
+// Filter retail companies
+
+const retailCompanies = companies.filter(function(company) {
+  if(company.category === 'Retail') {
+    return true;
+  }
+});
+
+const retailCompanies = companies.filter(company => company.category === 'Retail');
+
+// Get 80s companies
+
+const eightiesCompanies = companies.filter(company => (company.start >= 1980 && company.start < 1990));
+
+// Get companies that lasted 10 years or more
+
+const lastedTenYears = companies.filter(company => (company.end - company.start >= 10));
+
+// map
+
+Create array of company names
+const companyNames = companies.map(function(company) {
+  return company.name;
+});
+
+const testMap = companies.map(function(company) {
+  return `${company.name} [${company.start} - ${company.end}]`;
+});
+
+const testMap = companies.map(company => `${company.name} [${company.start} - ${company.end}]`);
+
+const ageMap = ages
+  .map(age => Math.sqrt(age))
+  .map(age => age * 2);
+
+
+
+sort
+
+Sort companies by start year
+
+const sortedCompanies  = companies.sort(function(c1, c2) {
+  if(c1.start > c2.start) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
+
+const sortedCompanies = companies.sort((a, b) => (a.start > b.start ? 1 : -1));
+
+Sort ages
+const sortAges = ages.sort((a, b) => a - b);
+
+console.log(sortAges);
+
+
+reduce
+
+let ageSum = 0;
+for(let i = 0; i < ages.length; i++) {
+  ageSum += ages[i];
+}
+
+const ageSum = ages.reduce(function(total, age) {
+  return total + age;
+}, 0);
+
+const ageSum = ages.reduce((total, age) => total + age, 0);
+
+// Get total years for all companies
+
+const totalYears = companies.reduce(function(total, company) {
+  return total + (company.end - company.start);
+}, 0);
+
+const totalYears = companies.reduce((total, company) => total + (company.end - company.start), 0);
+
+// Combine Methods
+
+const combined = ages
+  .map(age => age * 2)
+  .filter(age => age >= 40)
+  .sort((a, b) => a - b)
+  .reduce((a, b) => a + b, 0);
+
+console.log(combined);
+
+
+
+
